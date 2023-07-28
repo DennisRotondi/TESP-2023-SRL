f1cf22282b10a78cc67142e3c9d7d6680974b845
Marco Trentini <marcotr@ethz.ch>
2023-07-28 09:18:11 +0200

    bugfixes, no sleep yet

--- ws/src/tesp_server/src/server.py
+++ ws/src/tesp_server/src/server.py
@@ -3,45 +3,50 @@ import rospy
 import paramiko
 
 from std_msgs.msg import String
- 
-def turn_left(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(-50))"')
-    time.sleep(time)
-
-def turn_right(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(50))"')
-    time.sleep(time)
-
-def move_forward(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(50))"')
-    time.sleep(time)
-
-def move_backward(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(-50))"')
-    time.sleep(time)
-
-def callback(data, robot_connection):
-    if data.data == 'right':
-        turn_right(robot_connection)
-    elif data.data == 'left':
-        turn_left(robot_connection)
-    elif data.data == 'forward':
-        move_forward(robot_connection)
-    elif data.data == 'backward':
-        move_backward(robot_connection)
-    print(data.data)
 
 def connect_to_robot(ip,username,password):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, username=username, password=password)
     return client
+
+robot_ip = '10.42.0.252'
+robot_connection_1 = connect_to_robot(robot_ip,'robot','maker')
+
+ 
+def turn_left(robot_connection, time = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(-50))"')
+
+def turn_right(robot_connection, time = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(50))"')
+
+def move_forward(robot_connection, time = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(50))"')
+
+def move_backward(robot_connection, time = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(-50))"')
+
+def callback(data):
+    message = data.data
+    #log message to ros console
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", message)
+    print(message)
+    if message == 'right':
+        turn_right(robot_connection_1)
+    elif message == 'left':
+        turn_left(robot_connection_1)
+    elif message == 'forward':
+        move_forward(robot_connection_1)
+    elif message == 'backward':
+        move_backward(robot_connection_1)
+    print(message)
+
+
 def disconnect_from_robot(robot_connection):
     robot_connection.close()
 
 if __name__ == '__main__':
     rospy.init_node('server', anonymous=True)
-    robot_connection_1 = connect_to_robot('10.240.20.66','robot','maker')
     # pub = rospy.Publisher('action_tesp', String, queue_size=10)
-    rospy.Subscriber("action_tesp", String, callback(robot_connection_1))
+    rospy.Subscriber("action_tesp", String, callback)
     rospy.spin()
