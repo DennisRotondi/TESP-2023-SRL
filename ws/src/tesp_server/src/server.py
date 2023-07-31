c6586ab873e22dc0bd445732a0e59b828ddac0ff
Marco Trentini <marcotr@ethz.ch>
2023-07-31 08:10:37 +0200

    add timing to controls

--- ws/src/tesp_server/src/server.py
+++ ws/src/tesp_server/src/server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import rospy
 import paramiko
+import time
+import re
 
 from std_msgs.msg import String
 
@@ -8,38 +10,61 @@ def connect_to_robot(ip,username,password):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, username=username, password=password)
+    rospy.loginfo(rospy.get_caller_id() + "Client: %s", client)
     return client
 
 robot_ip = '10.42.0.252'
+# robot_ip = '10.240.20.63'
+# robot_ip = '192.168.10.103'
 robot_connection_1 = connect_to_robot(robot_ip,'robot','maker')
-
  
-def turn_left(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(-50))"')
+def turn_left(robot_connection, timer = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(-50));"')
+    time.sleep(timer)
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(0), SpeedPercent(0));"')
 
-def turn_right(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(50))"')
+def turn_right(robot_connection, timer = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(50));"')
+    time.sleep(timer)
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(0), SpeedPercent(0));"')
 
-def move_forward(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(50))"')
+def move_forward(robot_connection, timer = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(50), SpeedPercent(50));"')
+    time.sleep(timer)
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(0), SpeedPercent(0));"')
 
-def move_backward(robot_connection, time = 1):
-    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(-50))"')
+def move_backward(robot_connection, timer = 1):
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(-50), SpeedPercent(-50));"')
+    time.sleep(timer)
+    robot_connection.exec_command('python3 -c "from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent; tank_drive = MoveTank(OUTPUT_A, OUTPUT_B); tank_drive.on(SpeedPercent(0), SpeedPercent(0));"')
+
+import re
 
 def callback(data):
     message = data.data
-    #log message to ros console
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", message)
-    print(message)
-    if message == 'right':
+    time_match = re.match(r'(\w+)(\d+)', message)
+    
+    if time_match:
+        direction, time_ms = time_match.groups()
+        time_s = int(time_ms) / 1000.0
+    else:
+        direction = message
+        time_s = 0.0
+
+    if direction == 'right':
         turn_right(robot_connection_1)
-    elif message == 'left':
+    elif direction == 'left':
         turn_left(robot_connection_1)
-    elif message == 'forward':
+    elif direction == 'up':
         move_forward(robot_connection_1)
-    elif message == 'backward':
+    elif direction == 'down':
         move_backward(robot_connection_1)
+
+    if time_s > 0:
+        delay_ms(robot_connection_1, time_s)
+
     print(message)
+
 
 
 def disconnect_from_robot(robot_connection):
